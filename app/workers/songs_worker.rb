ca6861cf76330d5744b44b10b206c575c1879e36
ronald vilorio <soccer23415@gmail.com>
2018-06-16 19:42:11 +0000

    automatically backed up by learn

--- app/workers/songs_worker.rb
+++ app/workers/songs_worker.rb
@@ -3,6 +3,7 @@ class SongsWorker
   include SideKiq::Worker
 
   def perform(songs_file)
-
+    CSV.foreach(songs_file, headers: true) do |lead|
+      Customer.create(email: lead[0], first_name: lead[1], last_name: lead[2])
   end
 end

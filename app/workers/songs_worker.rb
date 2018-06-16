3b95eaf9f9e345fb14358052ef9ce859c292eb90
ronald vilorio <soccer23415@gmail.com>
2018-06-16 19:42:38 +0000

    automatically backed up by learn

--- app/workers/songs_worker.rb
+++ app/workers/songs_worker.rb
@@ -4,7 +4,6 @@ class SongsWorker
 
   def perform(songs_file)
     binding.pry
-    CSV.foreach(songs_file, headers: true) do |lead|
-      Customer.create(email: lead[0], first_name: lead[1], last_name: lead[2])
+    
   end
 end
